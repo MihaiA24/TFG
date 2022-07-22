a990334fe916e5d3200b35a7519f87f7f02d1ab2
Mihai Aftinescu <93051145+mihaia24@users.noreply.github.com>
2022-07-22 17:44:49 +0200

    removed unnecesary code

--- code/compare_results.py
+++ code/compare_results.py
@@ -19,7 +19,6 @@ args = sys.argv
 path = args[1]
 
 origImg = [None] * test_length 
-# origImg = dict()
 
 ads = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
        'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
@@ -31,20 +30,14 @@ for index,item in enumerate(tqdm(images)):
     origImg[index] = arr
 
 
-# resultImg = [None] * test_length 
 resultImg = dict()
-# Get results from outputs
-# for 
+
 lst = glob.glob(path + '*/10.*')
 lst = natsorted(lst)
 print('Total processed: ',len(lst))
 for index, item in enumerate(tqdm(lst)):
-    # print(item,' ',item.split('/')[-2],' ',item.split('6. ')[-1].split('.jpg')[0])
     resultImg[int(item.split('/')[-2])] = list(item.split('10. ')[-1].split('.jpg')[0])
 
-# print(origImg[12344])
-# print(resultImg.get(12345))
-# print(images[12344])
 
 iguales = [None] * test_length 
 maxIndex = 0
@@ -60,19 +53,3 @@ for index,item in enumerate(tqdm(origImg)):
 counter=collections.Counter(iguales)
 
 print(counter)
-
-
-# caractersIguales = [None] * (test_length * 6)
-# maxIndex = 0
-# for index, item in enumerate(tqdm(resultImg)):
-#     if item is not None:
-#         for index2, item2 in enumerate(item):
-#             if origImg[index][index2] == item2:
-#                 caractersIguales[maxIndex] = True
-#             else:
-#                 caractersIguales[maxIndex] = False
-#             maxIndex = maxIndex + 1
-
-
-# counter=collections.Counter(caractersIguales)
-# print(counter)
